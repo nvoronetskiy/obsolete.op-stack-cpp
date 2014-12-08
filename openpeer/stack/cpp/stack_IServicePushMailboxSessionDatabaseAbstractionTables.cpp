52c5d60fa5474c38276648af06122424f36bddb1
Robin Raymond <robin@hookflash.com>
2014-12-08 00:29:06 -0500

    - Implement push messaging database table and record structure https://github.com/openpeer/opios-production/issues/199

---
+++ openpeer/stack/cpp/stack_IServicePushMailboxSessionDatabaseAbstractionTables.cpp
@@ -0,0 +1,111 @@
+/*
+
+ Copyright (c) 2014, Hookflash Inc.
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ The views and conclusions contained in the software and documentation are those
+ of the authors and should not be interpreted as representing official policies,
+ either expressed or implied, of the FreeBSD Project.
+
+ */
+
+#include <openpeer/stack/internal/stack_IServicePushMailboxSessionDatabaseAbstractionTables.h>
+
+namespace openpeer { namespace stack { ZS_DECLARE_SUBSYSTEM(openpeer_stack) } }
+
+namespace openpeer
+{
+  namespace stack
+  {
+    namespace internal
+    {
+      //-----------------------------------------------------------------------
+      //-----------------------------------------------------------------------
+      //-----------------------------------------------------------------------
+      //-----------------------------------------------------------------------
+      #pragma mark
+      #pragma mark IServicePushMailboxSessionDatabaseAbstractionTables
+      #pragma mark
+
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::version = "version";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::keyID = "keyID";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::lastDownloadedVersionForFolders = "lastDownloadedVersionForFolders";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::uniqueID = "uniqueID";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::folderName = "folderName";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::serverVersion = "serverVersion";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::downloadedVersion = "downloadedVersion";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::totalUnreadMessages = "totalUnreadMessages";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::totalMessages = "totalMessages";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::updateNext = "updateNext";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::indexFolderRecord = "indexFolderRecord";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::messageID = "messageID";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::removedFlag = "removedFlag";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::to = "to";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::from = "from";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::cc = "cc";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::bcc = "bcc";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::type = "type";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::mimeType = "mimeType";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::encoding = "encoding";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::pushType = "pushType";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::pushInfo = "pushInfo";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::sent = "sent";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::expires = "expires";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::encryptedDataLength = "encryptedDataLength";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::encryptedData = "encryptedData";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::decryptedData = "decryptedData";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::encryptedFileName = "encryptedFileName";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::decryptedFileName = "decryptedFileName";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::hasEncryptedData = "hasEncryptedData";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::hasDecryptedData = "hasDecryptedData";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::downloadedEncryptedData = "downloadedEncryptedData";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::processedKey = "processedKey";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::decryptKeyID = "decryptKeyID";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::decryptLater = "decryptLater";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::decryptFailure = "decryptFailure";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::needsNotification = "needsNotification";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::indexMessageRecord = "indexMessageRecord";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::flag = "flag";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::uri = "uri";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::errorCode = "errorCode";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::errorReason = "errorReason";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::remoteFolder = "remoteFolder";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::copyToSent = "copyToSent";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::subscribeFlags = "subscribeFlags";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::listID = "listID";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::needsDownload = "needsDownload";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::failedDownload = "failedDownload";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::indexListRecord = "indexListRecord";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::order = "order";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::keyDomain = "keyDomain";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::rsaPassphrase = "rsaPassphrase";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::dhPassphrase = "dhPassphrase";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::dhStaticPrivateKey = "dhStaticPrivateKey";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::dhStaticPublicKey = "dhStaticPublicKey";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::listSize = "listSize";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::totalWithDHPassphraseSet = "totalWithDHPassphraseSet";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::activeUntil = "activeUntil";
+      const char *IServicePushMailboxSessionDatabaseAbstractionTables::passphrase = "passphrase";
+    }
+  }
+}
--- openpeer/stack/internal/stack_IServicePushMailboxSessionDatabaseAbstractionTables.h
+++ openpeer/stack/internal/stack_IServicePushMailboxSessionDatabaseAbstractionTables.h
@@ -33,8 +33,6 @@
 
 #include <openpeer/stack/internal/types.h>
 
-#include <openpeer/stack/internal/stack_IServicePushMailboxSessionDatabaseAbstraction.h>
-
 #include <easySQLite/SqlField.h>
 #include <easySQLite/SqlTable.h>
 #include <easySQLite/SqlRecord.h>
@@ -46,10 +44,10 @@ namespace openpeer
   {
     namespace internal
     {
-      //-------------------------------------------------------------------------
-      //-------------------------------------------------------------------------
-      //-------------------------------------------------------------------------
-      //-------------------------------------------------------------------------
+      //-----------------------------------------------------------------------
+      //-----------------------------------------------------------------------
+      //-----------------------------------------------------------------------
+      //-----------------------------------------------------------------------
       #pragma mark
       #pragma mark IServicePushMailboxSessionDatabaseAbstractionTables
       #pragma mark
@@ -61,17 +59,276 @@ namespace openpeer
         ZS_DECLARE_TYPEDEF_PTR(sql::Record, SqlRecord)
         ZS_DECLARE_TYPEDEF_PTR(sql::Value, SqlValue)
 
-        //-----------------------------------------------------------------------
+        static const char *version;
+        static const char *keyID;
+        static const char *lastDownloadedVersionForFolders;
+        static const char *uniqueID;
+        static const char *folderName;
+        static const char *serverVersion;
+        static const char *downloadedVersion;
+        static const char *totalUnreadMessages;
+        static const char *totalMessages;
+        static const char *updateNext;
+        static const char *indexFolderRecord;
+        static const char *messageID;
+        static const char *removedFlag;
+        static const char *to;
+        static const char *from;
+        static const char *cc;
+        static const char *bcc;
+        static const char *type;
+        static const char *mimeType;
+        static const char *encoding;
+        static const char *pushType;
+        static const char *pushInfo;
+        static const char *sent;
+        static const char *expires;
+        static const char *encryptedDataLength;
+        static const char *encryptedData;
+        static const char *decryptedData;
+        static const char *encryptedFileName;
+        static const char *decryptedFileName;
+        static const char *hasEncryptedData;
+        static const char *hasDecryptedData;
+        static const char *downloadedEncryptedData;
+        static const char *processedKey;
+        static const char *decryptKeyID;
+        static const char *decryptLater;
+        static const char *decryptFailure;
+        static const char *needsNotification;
+        static const char *indexMessageRecord;
+        static const char *flag;
+        static const char *uri;
+        static const char *errorCode;
+        static const char *errorReason;
+        static const char *remoteFolder;
+        static const char *copyToSent;
+        static const char *subscribeFlags;
+        static const char *listID;
+        static const char *needsDownload;
+        static const char *failedDownload;
+        static const char *indexListRecord;
+        static const char *order;
+        static const char *keyDomain;
+        static const char *rsaPassphrase;
+        static const char *dhPassphrase;
+        static const char *dhStaticPrivateKey;
+        static const char *dhStaticPublicKey;
+        static const char *listSize;
+        static const char *totalWithDHPassphraseSet;
+        static const char *activeUntil;
+        static const char *passphrase;
+
+
+        //---------------------------------------------------------------------
         static SqlField *Version()
         {
           static SqlField table[] = {
             SqlField(sql::FIELD_KEY),
-            SqlField("version", sql::type_int, sql::flag_not_null),
+            SqlField(version, sql::type_int, sql::flag_not_null),
             SqlField(sql::DEFINITION_END)
           };
           return table;
         };
-        
+
+        //---------------------------------------------------------------------
+        static SqlField *Settings()
+        {
+          static SqlField table[] = {
+            SqlField(sql::FIELD_KEY),
+            SqlField(lastDownloadedVersionForFolders, sql::type_text, sql::flag_not_null),
+            SqlField(sql::DEFINITION_END)
+          };
+          return table;
+        };
+
+        //---------------------------------------------------------------------
+        static SqlField *Folder()
+        {
+          static SqlField table[] = {
+            SqlField(sql::FIELD_KEY),
+            SqlField(uniqueID, sql::type_text, sql::flag_not_null | sql::flag_unique),
+            SqlField(folderName, sql::type_text, sql::flag_not_null | sql::flag_unique),
+            SqlField(serverVersion, sql::type_text, sql::flag_not_null),
+            SqlField(downloadedVersion, sql::type_text, sql::flag_not_null),
+            SqlField(totalUnreadMessages, sql::type_int, sql::flag_not_null),
+            SqlField(totalMessages, sql::type_int, sql::flag_not_null),
+            SqlField(updateNext, sql::type_int, sql::flag_not_null),
+            SqlField(sql::DEFINITION_END)
+          };
+          return table;
+        };
+
+        //---------------------------------------------------------------------
+        static SqlField *FolderMessage()
+        {
+          static SqlField table[] = {
+            SqlField(sql::FIELD_KEY),
+            SqlField(indexFolderRecord, sql::type_int, sql::flag_not_null),
+            SqlField(messageID, sql::type_text, sql::flag_not_null),
+            SqlField(sql::DEFINITION_END)
+          };
+          return table;
+        };
+
+        //---------------------------------------------------------------------
+        static SqlField *FolderVersionedMessaged()
+        {
+          static SqlField table[] = {
+            SqlField(sql::FIELD_KEY),
+            SqlField(indexFolderRecord, sql::type_int, sql::flag_not_null),
+            SqlField(messageID, sql::type_text, sql::flag_not_null),
+            SqlField(removedFlag, sql::type_bool, sql::flag_not_null),
+            SqlField(sql::DEFINITION_END)
+          };
+          return table;
+        };
+
+        //---------------------------------------------------------------------
+        static SqlField *Message()
+        {
+          static SqlField table[] = {
+            SqlField(sql::FIELD_KEY),
+            SqlField(indexFolderRecord, sql::type_int, sql::flag_not_null),
+            SqlField(messageID, sql::type_text, sql::flag_not_null),
+            SqlField(serverVersion, sql::type_text, sql::flag_not_null),
+            SqlField(downloadedVersion, sql::type_text, sql::flag_not_null),
+            SqlField(to, sql::type_text, sql::flag_not_null),
+            SqlField(from, sql::type_text, sql::flag_not_null),
+            SqlField(cc, sql::type_text, sql::flag_not_null),
+            SqlField(bcc, sql::type_text, sql::flag_not_null),
+            SqlField(type, sql::type_text, sql::flag_not_null),
+            SqlField(mimeType, sql::type_text, sql::flag_not_null),
+            SqlField(encoding, sql::type_text, sql::flag_not_null),
+            SqlField(pushType, sql::type_text, sql::flag_not_null),
+            SqlField(pushInfo, sql::type_text, sql::flag_not_null),
+            SqlField(sent, sql::type_int, sql::flag_not_null),
+            SqlField(expires, sql::type_int, sql::flag_not_null),
+            SqlField(encryptedDataLength, sql::type_int, sql::flag_not_null),
+            SqlField(encryptedData, sql::type_text, sql::flag_not_null),
+            SqlField(decryptedData, sql::type_text, sql::flag_not_null),
+            SqlField(encryptedFileName, sql::type_text, sql::flag_not_null),
+            SqlField(decryptedFileName, sql::type_text, sql::flag_not_null),
+            SqlField(hasEncryptedData, sql::type_bool, sql::flag_not_null),
+            SqlField(hasDecryptedData, sql::type_bool, sql::flag_not_null),
+            SqlField(downloadedEncryptedData, sql::type_bool, sql::flag_not_null),
+            SqlField(processedKey, sql::type_bool, sql::flag_not_null),
+            SqlField(decryptKeyID, sql::type_text, sql::flag_not_null),
+            SqlField(decryptLater, sql::type_bool, sql::flag_not_null),
+            SqlField(decryptFailure, sql::type_bool, sql::flag_not_null),
+            SqlField(needsNotification, sql::type_bool, sql::flag_not_null),
+            SqlField(sql::DEFINITION_END)
+          };
+          return table;
+        };
+
+        //---------------------------------------------------------------------
+        static SqlField *MessageDeliveryState()
+        {
+          static SqlField table[] = {
+            SqlField(sql::FIELD_KEY),
+            SqlField(indexMessageRecord, sql::type_int, sql::flag_not_null),
+            SqlField(flag, sql::type_text, sql::flag_not_null),
+            SqlField(uri, sql::type_text, sql::flag_not_null),
+            SqlField(errorCode, sql::type_int, sql::flag_not_null),
+            SqlField(errorReason, sql::type_text, sql::flag_not_null),
+            SqlField(sql::DEFINITION_END)
+          };
+          return table;
+        };
+
+        //---------------------------------------------------------------------
+        static SqlField *PendingDeliveryMessage()
+        {
+          static SqlField table[] = {
+            SqlField(sql::FIELD_KEY),
+            SqlField(indexMessageRecord, sql::type_int, sql::flag_not_null),
+            SqlField(remoteFolder, sql::type_text, sql::flag_not_null),
+            SqlField(copyToSent, sql::type_bool, sql::flag_not_null),
+            SqlField(subscribeFlags, sql::type_int, sql::flag_not_null),
+            SqlField(encryptedDataLength, sql::type_int, sql::flag_not_null),
+            SqlField(sql::DEFINITION_END)
+          };
+          return table;
+        };
+
+        //---------------------------------------------------------------------
+        static SqlField *List()
+        {
+          static SqlField table[] = {
+            SqlField(sql::FIELD_KEY),
+            SqlField(listID, sql::type_text, sql::flag_not_null),
+            SqlField(needsDownload, sql::type_bool, sql::flag_not_null),
+            SqlField(failedDownload, sql::type_bool, sql::flag_not_null),
+            SqlField(sql::DEFINITION_END)
+          };
+          return table;
+        };
+
+        //---------------------------------------------------------------------
+        static SqlField *ListURI()
+        {
+          static SqlField table[] = {
+            SqlField(sql::FIELD_KEY),
+            SqlField(indexListRecord, sql::type_int, sql::flag_not_null),
+            SqlField(order, sql::type_int, sql::flag_not_null),
+            SqlField(uri, sql::type_text, sql::flag_not_null),
+            SqlField(sql::DEFINITION_END)
+          };
+          return table;
+        };
+
+        //---------------------------------------------------------------------
+        static SqlField *KeyDomain()
+        {
+          static SqlField table[] = {
+            SqlField(sql::FIELD_KEY),
+            SqlField(keyDomain, sql::type_int, sql::flag_not_null),
+            SqlField(dhStaticPrivateKey, sql::type_text, sql::flag_not_null),
+            SqlField(dhStaticPublicKey, sql::type_text, sql::flag_not_null),
+            SqlField(sql::DEFINITION_END)
+          };
+          return table;
+        };
+
+
+        //---------------------------------------------------------------------
+        static SqlField *SendingKey()
+        {
+          static SqlField table[] = {
+            SqlField(sql::FIELD_KEY),
+            SqlField(keyID, sql::type_text, sql::flag_not_null),
+            SqlField(uri, sql::type_text, sql::flag_not_null),
+            SqlField(rsaPassphrase, sql::type_text, sql::flag_not_null),
+            SqlField(dhPassphrase, sql::type_text, sql::flag_not_null),
+            SqlField(keyDomain, sql::type_int, sql::flag_not_null),
+            SqlField(dhStaticPrivateKey, sql::type_text, sql::flag_not_null),
+            SqlField(dhStaticPublicKey, sql::type_text, sql::flag_not_null),
+            SqlField(listSize, sql::type_int, sql::flag_not_null),
+            SqlField(totalWithDHPassphraseSet, sql::type_int, sql::flag_not_null),
+            SqlField(activeUntil, sql::type_int, sql::flag_not_null),
+            SqlField(expires, sql::type_int, sql::flag_not_null),
+            SqlField(sql::DEFINITION_END)
+          };
+          return table;
+        };
+
+        //---------------------------------------------------------------------
+        static SqlField *ReceivingKey()
+        {
+          static SqlField table[] = {
+            SqlField(sql::FIELD_KEY),
+            SqlField(keyID, sql::type_text, sql::flag_not_null),
+            SqlField(uri, sql::type_text, sql::flag_not_null),
+            SqlField(passphrase, sql::type_text, sql::flag_not_null),
+            SqlField(keyDomain, sql::type_int, sql::flag_not_null),
+            SqlField(dhStaticPrivateKey, sql::type_text, sql::flag_not_null),
+            SqlField(dhStaticPublicKey, sql::type_text, sql::flag_not_null),
+            SqlField(expires, sql::type_int, sql::flag_not_null),
+            SqlField(sql::DEFINITION_END)
+          };
+          return table;
+        };
       };
     }
   }
